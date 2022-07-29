efa444ef7c4e712383cfa2a00a07d7d2cde048bd
madu <liuzzieduarda@gmail.com>
2022-07-28 23:15:51 -0300

    review - js

---
+++ review.js
@@ -0,0 +1,11 @@
+/*Caminho a percorrer! Aqui estão alguns dos principais conceitos para condicionais:
+
+Uma instrução if verifica uma condição e executará uma tarefa se essa condição for avaliada como verdadeira.
+As instruções if...else tomam decisões binárias e executam diferentes blocos de código com base em uma condição fornecida.
+Podemos adicionar mais condições usando instruções else if.
+Operadores de comparação, incluindo <, >, <=, >=, === e !== podem comparar dois valores.
+O operador lógico e, &&, ou “e”, verifica se ambas as expressões fornecidas são verdadeiras.
+O operador lógico ||, ou “ou”, verifica se qualquer expressão fornecida é verdadeira.
+O operador bang, !, alterna a veracidade e a falsidade de um valor.
+O operador ternário é uma abreviação para simplificar instruções concisas if...else.
+Uma instrução switch pode ser usada para simplificar o processo de escrever várias instruções else if. A palavra-chave break impede que os casos restantes sejam verificados e executados em uma instrução switch.*/
